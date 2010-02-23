a171d5b4970451db49892355f0ef37c165e65ec2
matt <matt.mets@cibomahto.com>
2010-02-23 03:19:04 -0500

    Add explicit timeout to serial port, fixing nil crash on linux

--- utilities/set_clock.rb
+++ utilities/set_clock.rb
@@ -1,15 +1,22 @@
 #!/usr/bin/ruby
 
+# TODO: Check that argv[0] contains a serial port
+# TODO: If above fails, print usage info.
+
 require "rubygems"
 # From http://ruby-serialport.rubyforge.org/
 require "serialport"
 
 puts "Connecting to Arduino on #{ARGV[0]}"
 SerialPort.open(ARGV[0], 9600, 8, 1, SerialPort::NONE) do |sp|
+  # Set serial port timeout to infinite (defaults to -1 on Linux)
+  sp.read_timeout = 0
+
   sleep 5
-  puts "Connected?"
-  buf = ""; while (! /.* POWER_ON\n/.match(buf)) do 
-    printf("%c", t = sp.getc)
+  puts "Waiting for POWER_ON message from Arduino"
+  buf = ""; while (! /.* POWER_ON.*\n/.match(buf)) do
+    t = sp.getc
+    printf("%c", t)
     buf << t
   end
   now = Time.now
--- utilities/update_acl.rb
+++ utilities/update_acl.rb
@@ -1,5 +1,16 @@
 #!/usr/bin/ruby
 
+# To use this, set:
+#  HACKPGH_GOOGLE_USER to your HackPGH google apps account
+#  HACKPGH_GOOGLE_KEY to your password for said account
+#  HACKPGH_GOOGLE_MEMBER_SHEET to the members worksheet key (it's part of the URL)
+
+# TODO: Check for existance of environment variables
+# TODO: Check that argv[0] contains a serial port
+# TODO: If either fails, print usage info.
+
+# TODO: Check that we can open the serial port, before bothering to go out to the net?
+
 require "rubygems"
 # From http://github.com/gimite/google-spreadsheet-ruby
 require "google_spreadsheet"
@@ -22,10 +33,14 @@ puts "About to write #{rfid_tags.size} RFID Tags"
 puts "Connecting to Arduino on port #{ARGV[0]}"
 
 SerialPort.open(ARGV[0], 9600, 8, 1, SerialPort::NONE) do |sp|
+  # Set serial port timeout to infinite (defaults to -1 on Linux)
+  sp.read_timeout = 0 
+
   sleep 5
-  puts "Connected?"
-  buf = ""; while (! /.* POWER_ON\n/.match(buf)) do 
-    printf("%c", t = sp.getc)
+  puts "Waiting for POWER_ON message from Arduino"
+  buf = ""; while (! /.* POWER_ON.*\n/.match(buf)) do
+    t = sp.getc
+    printf("%c", t)
     buf << t
   end
   sp.write 'U' 
